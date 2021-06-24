72de5283cf520ed33a0c20ebde701c4c49ade511
hai <haithngn@gmail.com>
2021-06-24 10:43:49 +0700

    - Adjust valid architects

---
+++ SwiftLinkPreview/SwiftLinkPreview.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLinkPreview.h
+//  SwiftLinkPreview
+//
+//  Created by Hai Nguyen on 6/24/21.
+//  Copyright © 2021 leocardz.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLinkPreview.
+FOUNDATION_EXPORT double SwiftLinkPreviewVersionNumber;
+
+//! Project version string for SwiftLinkPreview.
+FOUNDATION_EXPORT const unsigned char SwiftLinkPreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLinkPreview/PublicHeader.h>
+
+
